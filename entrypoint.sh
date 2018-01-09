4588499651417b05e7cc111ba51c23f9ede6f61b
agong1 <agong1@stumail.jccc.edu>
2018-01-09 17:14:21 +0800

    修改PORT
    修改PORT

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 cd /v2raybin
 echo -e -n '{"log":{"loglevel":"warning"},"inbound":{"protocol":"vmess","port":' > config.json
-echo -e -n '8088' >> config.json
+echo -e -n "$PORT" >> config.json
 echo -e -n ',"settings":{"clients":[{"id":"' >> config.json
 echo -e -n '9e53c877-af25-47ed-9549-118ed80996d1' >> config.json
 echo -e -n '","alterId":64,"security":"none"}]},"streamSettings":{"network":"ws"}},"inboundDetour":[],"outbound":{"protocol":"freedom","settings":{}}}' >> config.json
